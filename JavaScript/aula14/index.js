28d99d639c9cd323f2e9eb9d8237bee82cf6a8a1
VKURHT <victork113kv@gmail.com>
2025-01-18 19:32:05 -0300

    aula 15 js

--- JavaScript/aula14/index.js
+++ JavaScript/aula14/index.js
@@ -1,5 +1,17 @@
-let num1 = 1;
-let num2 = 2.5;
+let num1 = 0.7;
+let num2 = 0.1;
 
-console.log(num1.toString() + num2);
+num1 += num2;
+num1 += num2;
+num1 += num2;
+
+num1 = parseFloat(num1.toFixed(2))
+console.log(num1,Number.isInteger(num1))
+//console.log(num1.toString() + num2);
+//num1 = num1.toString();
+//console.log(num1.toString(2)) // mostra em binário
+//console.log(num1.toFixed(2)) // mostra a quantidade de casas depois da virgula
+//console.log(Number.isInteger(num1)); // mostra se o numero é inteiro
+//let temp = num1 * 'Olá';
+//console.log(isNaN(temp));
 
---
+++ JavaScript/aula15/index.js
@@ -0,0 +1,8 @@
+let num1 = 9.54578;
+//let num2 = Math.floor(num1); //arredonda pra baixo
+//let num2 = Math.ceil(num1); //arredonda pra cima
+//let num2 = Math.round(num1); //aredonda para o mais proximo
+//console.log(Math.max(1,2,3,4,5,-10,-50,-100,1500, 9, 8, 7, 6));
+//console.log(Math.min(1,2,3,4,5,-10,-50,-100,1500, 9, 8, 7, 6));
+//const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+//console.log(aleatorio)
